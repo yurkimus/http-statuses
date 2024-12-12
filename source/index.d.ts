edf80a04d161f091db6fa40cf0fbc8df9ae3c19c
yurkimus <yurkimus@gmail.com>
2024-12-12 05:10:34 +0100

    Add ResponseStatus interface

--- source/index.d.ts
+++ source/index.d.ts
@@ -1,15 +1,24 @@
 import type { Statuses, StatusTexts } from './enumeration.d.ts'
 
-interface ResponseStatusConstructor {
-  new(value: Statuses | StatusTexts): ResponseStatusConstructor
+interface ResponseStatus {
+  status: Statuses
+  statusText: StatusTexts
 
-  (value: Statuses | StatusTexts): ResponseStatusConstructor
+  toString(): string
+
+  readonly [Symbol.toStringTag]: 'ResponseStatus'
+}
+
+interface ResponseStatusConstructor {
+  new(value: Statuses | StatusTexts): ResponseStatus
+
+  (value: Statuses | StatusTexts): ResponseStatus
 
   has(value: Statuses | StatusTexts): boolean
 
   get(value: Statuses | StatusTexts): string | number
 
-  readonly prototype: typeof ResponseStatus
+  readonly prototype: ResponseStatus
 
   readonly [Symbol.toStringTag]: 'ResponseStatus'
 }
